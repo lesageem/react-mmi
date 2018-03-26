7a644aabe903ff958544c189cd92eb3b193e0fc7
Emma Lesage <emma.lesage@etu.univ-grenoble-alpes.fr>
2018-03-26 15:07:12 +0000

    Création du composant Machine

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -6,21 +6,24 @@
 import React from 'react';
 
 class Machine extends React.Component {
+    handleClick = () => {
+    console.log('this is:', this);
+      }
+  
     render() {
     // Dans tous les cas, afficher
-     
     return (
-      <div className='machine'
-      className={this.props.isActive ? "machine active" : "machine"}>
-      <h2> {this.props.title} | {this.props.isActive} </h2>
-      <button>
-      {this.props.isActive? 'true' : 'false'}
-      </button>
+    <div className='machine'
+    className={this.props.isActive ? "machine active" : "machine"}>
+    <h2> {this.props.title} | {this.props.isActive} </h2>
       
-     
+    <button onClick={this.handleClick}>
+    {this.props.isActive? 'true' : 'false'}
+    
+    </button>
       
-      </div>
-      )
+    </div>
+    )
   }
 }
 
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,28 +1,51 @@
-/**
- *
- * App.js
- *
- */
 import React from 'react';
 
-// On importe ici les composants qu'on veut afficher
 import Machine from '../../components/Machine.js';
 import Header from '../../components/Header.js';
 import Footer from '../../components/Footer.js';
 import CSS from '../../css/style.css';
-// On peut aussi importer du CSS de la meme facon.
 
-export default function App() {
-  return (
-    // Un return doit retourner un seul élément du DOM
-    // Si on veut afficher plusieurs éléments adjacents,
-    // On devra donc les encapsuler dans une DIV parente.
-    <div> 
-    <Header/> 
-    <Machine title = "Machine 1"  isActive button/>
-    <Machine title = "Machine 2"  isActive button/>
-    <Machine title = "Machine 3"  button/>
-    <Footer/>
-    </div>
-  );
+class App extends React.Component {
+  
+constructor(props) {
+  super(props)
+    this.state = {
+      machines:[
+       { 
+          name : "Machine à café", 
+          isActive : true}, 
+        { 
+          name : "Machine à thé ", 
+          isActive : false}, 
+        { 
+          name : "Machine à chocolat", 
+          isActive : true}
+      
+    ]
+    };
+  
 }
+  
+  
+  render() {
+    return (
+       <div> 
+        <Header/> 
+        {
+          this.state.machines.map(machine => 
+          //console.log(machine.name))
+          <Machine title={machine.name} 
+          isActive={machine.isActive} />
+          
+        // <Machine title ={this.state.machines[0].name}  isActive= {this.state.machines[0].isActive} />
+        //<Machine title ={this.state.machines[1].name}  isActive= {this.state.machines[1].isActive}/>
+        //<Machine title ={this.state.machines[2].name}  isActive= {this.state.machines[2].isActive}/>
+        )}
+       
+        <Footer/>
+        </div>
+      );
+  }
+}
+
+export default App;
