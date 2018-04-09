21855dc4d511908e9e2b8536068e48c1ca2ee74c
Emma Lesage <emma.lesage@etu.univ-grenoble-alpes.fr>
2018-04-09 09:42:00 +0000

    toggle

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -4,29 +4,39 @@
  *
  */
 import React from 'react';
-
+import Toggle from 'react-toggle';
 
 
 
 class Machine extends React.Component {
-
+// Méthode lancée au clic sur le bouton "Toggle"
     
   onhandleClick() {
+// On envoie l'index à la méthode qui changera la valeur
+// La méthode et son argument sont tous les deux accessibles
+// via des props qui ont été passées au composant
   this.props.handleStatusChange(this.props.index);
   }
     
     
+ 
    render() {
     return (
     <div className='machine'
-    className={this.props.isActive ? "machine active" : "machine"}>
+    className={this.props.isActive ? "machine active" : "machine"}>  {/* Si isActive passée en props est à true, ajouter la classe "active" */}
     <h2> {this.props.name} </h2>
-     <button onClick={(e) => this.onhandleClick(e)} type="button" className="btn">
-        Activer
-    </button>
-      
-    </div>
-    )
+     {/* On appelle handleClick avec (e) pour pouvoir accéder à this*/}
+   {/* <button onClick={(e) => this.onhandleClick(e)} type="button" className="btn">
+      {this.props.isActive ? "Désactiver" : "activer"}
+    </button>*/}
+ <label>
+  <Toggle
+    defaultChecked={this.props.isActive}
+    onClick={(e) => this.onhandleClick(e)} />
+ 
+</label>
+  </div>
+    );
   }
 }
 
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,59 +1,67 @@
 import React from 'react';
-
+// Importe les composants qu'on veut afficher
 import Machine from '../../components/Machine.js';
 import Header from '../../components/Header.js';
 import Footer from '../../components/Footer.js';
 import CSS from '../../css/style.css';
-
+import Toggle from 'react-toggle';
 
 class App extends React.Component {
 
 constructor(props) {
-  super(props)
-   this.handleStatusChange = this.handleStatusChange.bind(this);
-    this.state = {
-      machines:[
-       {id:0,
-        name : "Machine à café", 
-        isActive : true}, 
-        { id:1,
-        name : "Machine à thé ", 
-        isActive : false}, 
-        { id:2,
-        name : "Machine à chocolat", 
-        isActive : true
-        },
-        { id:3,
-        name : "Machine à eau", 
-        isActive : false
-        }
-      ]
-    };
-   }
-   handleStatusChange(key) {
-      const machines = { ...this.state.machines };
-      machines[key].isActive = true;
-      console.log({ machines });
-      this.setState({ machines });
-  }
+  super(props);
+  this.handleStatusChange = this.handleStatusChange.bind(this);
+  this.state = {
+    machines:[
+     {id:0,
+      name : "Machine à café", 
+      isActive : true}, 
+      { id:1,
+      name : "Machine à thé ", 
+      isActive : false}, 
+      { id:2,
+      name : "Machine à chocolat", 
+      isActive : true
+      },
+      { id:3,
+      name : "Machine à eau", 
+      isActive : false
+      }
+    ]
+  };
+}
+ // Méthode pour activer une machine
+handleStatusChange(key) {
+// 1. On copie le state existant
+  const machines = { ...this.state.machines };
+  console.log(machines[key].isActive)
+// 2. On modifie le status de CETTE machine
+  machines[key].isActive = !machines[key].isActive;
+// On verifie dans la console
+  console.log(machines[key].isActive);
+// 3. On applique cette nouvelle collection au state
+  this.setState({ machines });
+}
   
-  render() {
-    return (
-      <div className="main">
-        <Header/>
-          <div className="machines-list">
-            {}
-            {
-            Object
-              .keys(this.state.machines)
-              .map(key =>
-                <Machine name={this.state.machines[key].name}
-                         key={this.state.machines[key].id}
-                         index={this.state.machines[key].id}
-                         handleStatusChange={this.handleStatusChange}
-                         isActive={this.state.machines[key].isActive}/>
-              )}
-          </div>
+render() {
+  return (
+    <div className="main">
+      <Header/>
+       {/*Conteneur de notre liste*/}
+      <div className="machines-list">
+        {/*Boucle sur notre collection de machines*/}
+        {
+          Object
+            .keys(this.state.machines)
+            .map(key =>
+             // Le composant Machine s'affichera autant de fois qu'il y a d'objets dans la collection.
+            <Machine name={this.state.machines[key].name}
+            key={this.state.machines[key].id}
+            index={this.state.machines[key].id}
+            handleStatusChange={this.handleStatusChange}
+            isActive={this.state.machines[key].isActive}/>
+            )}
+        </div>
         <Footer/>
       </div>
     );
